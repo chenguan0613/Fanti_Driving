fed8faa6db318ac53bd39ebf1793301e9e4fde6b
Fovir <fovir@disroot.org>
2026-06-16 17:45:39 +0800

    fix: fix the default value of `float` variables
    Signed-off-by: Fovir <fovir@disroot.org>

--- src/features/frame_schema.py
+++ src/features/frame_schema.py
@@ -8,8 +8,8 @@ class FrameFeature:
     left_EAR: float = 0
     right_EAR: float = 0
     eye_closed: int = 0
-    mounth_open_ratio: float = 0
-    head_pitch: float = 0
-    head_yaw: float = 0
-    gaze_x: float = 0
-    gaze_y: float = 0
+    mounth_open_ratio: float = 0.0
+    head_pitch: float = 0.0
+    head_yaw: float = 0.0
+    gaze_x: float = 0.0
+    gaze_y: float = 0.0
