7aa04d25c5404d10b35ab23e7e5009ff86e83287
Fovir <fovir@disroot.org>
2026-06-16 16:30:56 +0800

    chore: initial commit
    Signed-off-by: Fovir <fovir@disroot.org>

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "DevShell for Venv.";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      # Add packages here.
+      buildInputs = with pkgs; [
+        glib
+        libGL
+        libx11
+        libxcb
+        libxext
+        libxkbcommon
+        libxrender
+        python313
+        stdenv.cc.cc.lib
+        zlib
+        (pkgs.python3.withPackages (python-pkgs:
+          with python-pkgs; [
+            # Python packages:
+            # opencv4
+          ]))
+      ];
+      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+        pkgs.glib
+        pkgs.libGL
+        pkgs.libxkbcommon
+        pkgs.stdenv.cc.cc
+        pkgs.libx11
+        pkgs.libxext
+        pkgs.libxrender
+        pkgs.libxcb
+        pkgs.zlib
+      ];
+
+      # Shell hooks.
+      shellHook = ''
+        echo "Entering the development environment!"
+      '';
+    };
+  };
+}
