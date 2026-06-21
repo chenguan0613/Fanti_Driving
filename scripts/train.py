7f57a05d6be6667f6f972fbcb0bac9ed11781c93
Fovir <fovir@disroot.org>
2026-06-21 22:45:48 +0800

    feat: make model output path configurable
    Signed-off-by: Fovir <fovir@disroot.org>

--- scripts/train.py
+++ scripts/train.py
@@ -4,7 +4,7 @@ from src.train import Train
 def main():
     PATH = "./data/dataset/merge_five_enhanced.csv"
     t = Train(PATH)
-    t.train_and_eval()
+    t.train_and_eval("models/fatigue_model.pkl")
 
 
 if __name__ == "__main__":
