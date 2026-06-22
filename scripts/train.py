955f41c2b5974d499a455c1d1aaa9a4b28c521ab
Fovir <fovir@disroot.org>
2026-06-22 13:34:09 +0800

    refactor: remove `Train` from `train.py`
    Signed-off-by: Fovir <fovir@disroot.org>

--- scripts/train.py
+++ scripts/train.py
@@ -1,12 +1,8 @@
-from src.train import Train
 from src.train import HeuristicTrain
 
 
 def main():
     PATH = "./data/dataset/enhanced_merged.csv"
-    # train.py
-    t = Train(PATH)
-    t.train_and_eval("models/fatigue_model.pkl")
 
     print("=" * 20)
     print("After using heuristic learning to automatically select the features")
@@ -14,7 +10,7 @@ def main():
 
     # heuristic_train.py
     T = HeuristicTrain(PATH)
-    T.run()
+    T.run("models/heuristic_model.pkl")
 
 
 if __name__ == "__main__":
