64af3fe8e0dd729d054332f078960a8e5ec9538a
ChenGuan <3168966792@qq.com>
2026-06-23 17:57:08 +0800

    chore: add comments to train.py

--- scripts/train.py
+++ scripts/train.py
@@ -3,12 +3,7 @@ from src.train import HeuristicTrain
 
 def main():
     PATH = "./data/dataset/enhanced_merged.csv"
-
-    print("=" * 20)
-    print("After using heuristic learning to automatically select the features")
-    print("=" * 20)
-
-    # heuristic_train.py
+    # Using the dataset to train
     T = HeuristicTrain(PATH)
     T.run("models/heuristic_model.pkl")
 
