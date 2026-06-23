57425a7e56333e657210041cbe9391b26b7d0361
ChenGuan <3168966792@qq.com>
2026-06-23 18:21:06 +0800

    chore: modify the output path of build_csv.py

--- scripts/build_csv.py
+++ scripts/build_csv.py
@@ -3,7 +3,7 @@ from src.dataset import CSVBuilder
 
 def main():
     DATA_DIR = "data/"
-    OUTPUT_PATH = "data/dataset.csv"
+    OUTPUT_PATH = "data/dataset/dataset.csv"
 
     cb = CSVBuilder(DATA_DIR, OUTPUT_PATH)
     cb.build_csv()
